a699e680075120941102dd726f341419044cefad
akindutire <akinsamuel33@gmail.com>
2025-10-10 13:32:02 +0100

    moved from phpunit env to .env.testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,19 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\File;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Load .env.testing file if it exists
+        $testingEnvFile = base_path('.env.testing');
+        if (File::exists($testingEnvFile)) {
+            $dotenv = \Dotenv\Dotenv::createImmutable(base_path(), '.env.testing');
+            $dotenv->load();
+        }
+    }
 }
