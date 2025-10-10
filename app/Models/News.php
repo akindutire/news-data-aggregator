2b628b19556189f7e4acd4f9fde772f72697f119
akindutire <akinsamuel33@gmail.com>
2025-10-10 18:27:40 +0100

    added fillables

--- app/Models/News.php
+++ app/Models/News.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class News extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'url',
+        'category',
+        'published_at',
+        'source',
+        'author',
+        'content',
+        'description',
+        'image_url',
+        'article_remote_key',
+        'article_remote_source',
+    ];
+
+    protected $casts = [
+        'published_at' => 'datetime',
+    ];
 }
