9cfae25ab677dbdba07d52187fb0f8d2915cd403
akindutire <akinsamuel33@gmail.com>
2025-10-10 15:23:30 +0100

    added guardian unit tests

---
+++ tests/Unit/GuardianSourceUnitTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\PossibleNewsSource;
+use App\Services\Contracts\OchestrateProps;
+use App\Services\Factories\NewsSourceFactory;
+use Illuminate\Support\Facades\App;
+// use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
+
+class Guardian extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function test_guardian_news_can_fetch_data(): void
+    {
+        $this->assertContains(PossibleNewsSource::GUARDIAN->value, config('innoscripta.supported_news_sources'));
+
+        $guardianNews = (new NewsSourceFactory())->make(PossibleNewsSource::GUARDIAN->value);
+        $response = $guardianNews->fetchByPage(1,10);
+        $data = $response->json('response.results', []);
+
+        $this->assertIsArray($data);
+        $this->assertNotEmpty($data);
+    }
+
+    public function test_guardian_news_data_can_adapt_to_its_features(): void
+    {
+        $this->assertContains(PossibleNewsSource::GUARDIAN->value, config('innoscripta.supported_news_sources'));
+
+        $guardianNews = (new NewsSourceFactory())->make(PossibleNewsSource::GUARDIAN->value);
+        $response = $guardianNews->fetchByPage(1,10);
+        $data = $response->json('response.results', []);
+
+        $this->assertIsArray($data);
+        $this->assertNotEmpty($data);
+
+        $mappedData = $guardianNews->extractFeatures($data);
+
+        $this->assertIsArray($mappedData);
+        $this->assertNotEmpty($mappedData);
+
+        //test 1 of the items has the right keys
+        $item = $mappedData[0];
+        $features = $guardianNews->getFeaturesToExtract();
+        foreach($features as $feature) {
+            $this->assertArrayHasKey($feature, $item);
+        }
+    }
+
+    public function test_guardian_news_data_can_orchestrate_news_fetch(): void
+    {
+        $this->assertContains(PossibleNewsSource::GUARDIAN->value, config('innoscripta.supported_news_sources'));
+
+        $guardianNews = (new NewsSourceFactory())->make(PossibleNewsSource::GUARDIAN->value);
+
+        $props = new OchestrateProps;
+        $props->maxItems = 5;
+        $props->pageSize = 5;
+        $adaptedData = $guardianNews->orchestrate($props);
+
+        $this->assertIsArray($adaptedData);
+        $this->assertNotEmpty($adaptedData);
+
+        //test 1 of the items has the right keys
+        $item = $adaptedData[0];
+        $this->assertInstanceOf(\App\Models\ValueObject\NewsVO::class, $item);
+
+    }
+
+}
