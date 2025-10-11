66b5c70fa370c5ecb82c50e6db77efb2b34117cf
akindutire <akinsamuel33@gmail.com>
2025-10-11 12:48:37 +0100

    added setter for orderbyDir

--- app/Services/Contracts/AggregatorInterface.php
+++ app/Services/Contracts/AggregatorInterface.php
@@ -9,18 +9,27 @@ interface AggregatorInterface {
 }
 
 class FetchOpts extends stdClass {
-    public string $source = '';
+    public string|array $source = '';
     public string|array $category = '';
     public bool $distinct = false;
     public array $fields = [];
-    public array $orderBy = [];
+    public string $orderBy = '';
     public string $orderByDir = "ASC";
     public string $title = '';
-    public string $fromDate = '';
-    public string $toDate = '';
+    public \DateTime $fromDate;
+    public \DateTime $toDate;
     public string|array $author = '';
     public bool $shouldPaginate = true;
     public int $perPage = 25;
     public int $page = 1;
 
+    function _set(string $prop, $value) {
+        if ($prop == 'orderByDir') {
+            if (!in_array($value, ["ASC", "DESC"])) {
+                $value = "ASC";
+            }
+        }
+
+        $this->$prop = $value;
+    }
 }
