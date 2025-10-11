8cea8ea4a03a475a68ef49d4bf3367be5a77fd71
akindutire <akinsamuel33@gmail.com>
2025-10-11 07:44:18 +0100

    added newyorktimes strategy and its unit test

---
+++ app/Services/Concretes/ThirdParty/News/NewYorkTimes.php
@@ -0,0 +1,101 @@
+<?php
+namespace App\Services\Concretes\ThirdParty\News;
+
+use App\Models\ValueObject\NewsVO;
+use App\PossibleNewsSource;
+use App\Services\Contracts\OrchestrateProps;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
+class NewYorkTimes extends \App\Services\Abstracts\AbstractNewsSource
+{
+    private string $baseUrl = "https://api.nytimes.com/svc/news/v3/content/all";
+    private $source = PossibleNewsSource::NEWYORKTIMES->value;
+
+    public function getSource(): string
+    {
+        return $this->source;
+    }
+
+    public function featuresToExtract(): array {
+        return ['title', 'byline', 'slug_name', 'published_date', 'url', 'source', 'abstract', "subsection", "section", "des_facet"];
+    }
+
+    public function fetchByPage($page, $fetchSize) {
+        return Http::get($this->baseUrl . "/all.json", [
+            'source' => 'nyt',
+            'api-key' => env('NEWYORKTIMES_API_KEY'),
+            'limit' => $fetchSize,
+            'offset' => ($page-1)*$fetchSize,
+        ]);
+    }
+
+    /**
+     * @return NewsVO[]
+     */
+    public function orchestrate($property=null) : array {
+
+        if (is_null($property) ) {
+            $property = new OrchestrateProps;
+            $property->pageSize = 100;
+        }
+
+        // Pull data
+        $dataSet = [];
+
+        // Check orchestration state, get the last page fetched and continue from
+        // This step would keep news up to date without extreme overfetching
+        $orchState = $this->orchestrationState();
+        $page = $orchState?->last_fetched_page??1;
+
+        do{
+            $response = $this->fetchByPage($page, $property?->pageSize??100);
+
+            if ($response->ok()) {
+                $dataSet = [...$dataSet, ...$response->json('results', [])];
+            } else {
+                Log::error("Error fetching news02: ".$response->json('message',  'An error occured'));
+                break;
+            }
+
+            $totalPages = 5;
+            $page++;
+
+            // Stop news fetch when limit reached or exceeded
+            if($property?->maxItems??0 > 0 && count($dataSet) >= $property?->maxItems??0) {
+                break;
+            }
+        } while ($page < $totalPages);
+
+        // Create or Update state
+        if(count($dataSet) > 0) {
+            $this->updateOrchestrationState($page);
+        } else {
+            return [];
+        }
+
+        // filter/clean data
+        $dataSet = $this->extractFeatures($dataSet);
+
+
+        // adapt cleaned data to system features (news value object)
+        return array_map(
+            function($item) {
+                return (new NewsVO())
+                    ->setTitle($item['title'])
+                    ->setPublishedAt(new \DateTime($item['published_date']))
+                    ->setUrl($item['url'])
+                    ->setSource($this->getSource())
+                    ->setRemoteSource($item['source'])
+                    ->setRemoteId($item['slug_name'])
+                    ->setCategory($item['section'])
+                    ->setAuthor($item['byline'])
+                    ->setContent("")
+                    ->setDescription($item['abstract'])
+                    ->setImageUrl('');
+            },
+            $dataSet
+        );
+    }
+
+}
---
+++ tests/Unit/NewYorkTimesSourceUnitTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\PossibleNewsSource;
+use App\Services\Contracts\OrchestrateProps;
+use App\Services\Factories\NewsSourceFactory;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class NewYorkTimesSourceUnitTest extends TestCase
+{
+        use RefreshDatabase;
+
+    public function test_newyorktimes_news_can_fetch_data(): void
+    {
+        $this->assertContains(PossibleNewsSource::NEWYORKTIMES->value, config('innoscripta.supported_news_sources'));
+
+        $source = (new NewsSourceFactory())->make(PossibleNewsSource::NEWYORKTIMES->value);
+        $response = $source->fetchByPage(1,10);
+        $data = $response->json('results', []);
+
+        $this->assertIsArray($data);
+        $this->assertNotEmpty($data);
+    }
+
+    public function test_newyorktimes_news_data_can_adapt_to_its_features(): void
+    {
+        $this->assertContains(PossibleNewsSource::NEWYORKTIMES->value, config('innoscripta.supported_news_sources'));
+
+        $source = (new NewsSourceFactory())->make(PossibleNewsSource::NEWYORKTIMES->value);
+        $response = $source->fetchByPage(1,10);
+        $data = $response->json('results', []);
+
+        $this->assertIsArray($data);
+        $this->assertNotEmpty($data);
+
+        $mappedData = $source->extractFeatures($data);
+
+        $this->assertIsArray($mappedData);
+        $this->assertNotEmpty($mappedData);
+
+        //test 1 of the items has the right keys
+        $item = $mappedData[0];
+        $features = $source->getFeaturesToExtract();
+        foreach($features as $feature) {
+            $this->assertArrayHasKey($feature, $item);
+        }
+    }
+
+    public function test_newyorktimes_news_data_can_orchestrate_news_fetch(): void
+    {
+        $this->assertContains(PossibleNewsSource::NEWYORKTIMES->value, config('innoscripta.supported_news_sources'));
+
+        $source = (new NewsSourceFactory())->make(PossibleNewsSource::NEWSAPIORG->value);
+
+        $props = new OrchestrateProps;
+        $props->maxItems = 5;
+        $props->pageSize = 5;
+        $adaptedData = $source->orchestrate($props);
+
+        $this->assertIsArray($adaptedData);
+        $this->assertNotEmpty($adaptedData);
+
+        //test 1 of the items has the right keys
+        $item = $adaptedData[0];
+        $this->assertInstanceOf(\App\Models\ValueObject\NewsVO::class, $item);
+
+    }
+}
