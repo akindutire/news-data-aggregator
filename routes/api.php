4ce17bc0b90f8c634f862c09110715eb5b6598da
akindutire <akinsamuel33@gmail.com>
2025-10-11 11:14:34 +0100

    added exception handling

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,6 @@
 use App\Http\Controllers\NewsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('news/categories', [NewsController::class, 'allCategories']);
+Route::get('news/category', [NewsController::class, 'allCategories']);
 
 Route::get('news', [NewsController::class, 'get']);
